8ffb773f66d7ddeaba5c046b065e4cf70e9e8701
MarcosVel <marcosvelosodev@gmail.com>
2023-11-17 18:06:36 -0300

    chore: add icomoon and font loading

--- App.jsx
+++ App.jsx
@@ -1,12 +1,33 @@
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
+import { useCallback } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import CustomIcon from "./src/components/CustomIcon";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    IcoMoon: require("./src/assets/fonts/app_icons.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
+    <View style={styles.container} onLayout={onLayoutRootView}>
       <Text style={styles.text}>
         Open up App.js to start working on your app!
       </Text>
+      <CustomIcon name="like" size={32} />
       <StatusBar style="light" />
     </View>
   );
---
+++ src/components/CustomIcon.jsx
@@ -0,0 +1,8 @@
+import { createIconSetFromIcoMoon } from "@expo/vector-icons";
+import icoMoonConfig from "../../assets/icons/selection.json";
+
+export default createIconSetFromIcoMoon(
+  icoMoonConfig,
+  "IcoMoon",
+  "icomoon.ttf"
+);
