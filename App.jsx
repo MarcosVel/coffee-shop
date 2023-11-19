97774374dcb8f941612649b476adc7642fd8b70f
MarcosVel <marcosvelosodev@gmail.com>
2023-11-19 17:06:36 -0300

    refactor: adding poppins font ton expo useFonts

--- App.jsx
+++ App.jsx
@@ -8,6 +8,15 @@ SplashScreen.preventAutoHideAsync();
 export default function App() {
   const [fontsLoaded] = useFonts({
     IcoMoon: require("./src/assets/fonts/app_icons.ttf"),
+    "Poppins-Black": require("./src/assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Bold": require("./src/assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-ExtraBold": require("./src/assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-ExtraLight": require("./src/assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-Light": require("./src/assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Medium": require("./src/assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("./src/assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("./src/assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Thin": require("./src/assets/fonts/Poppins-Thin.ttf"),
   });
 
   useEffect(() => {
