9649016099a332f1ebf48baaf264a17ed578aacb
MarcosVel <marcosvelosodev@gmail.com>
2023-11-19 17:07:20 -0300

    feat: components for the header bar

---
+++ src/components/HeaderBar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { COLORS, FONTFAMILY, FONTSIZE, SPACING } from "../theme/theme";
+import GradientIcon from "./GradientIcon";
+import ProfilePic from "./ProfilePic";
+
+const HeaderBar = ({ title }) => {
+  return (
+    <View style={styles.container}>
+      <GradientIcon
+        name="menu"
+        color={COLORS.primaryLightGreyHex}
+        size={FONTSIZE.size_16}
+      />
+
+      <Text style={styles.text}>{title}</Text>
+
+      <ProfilePic />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    padding: SPACING.space_30,
+    paddingTop: SPACING.space_10,
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  text: {
+    fontFamily: FONTFAMILY.poppins_semibold,
+    fontSize: FONTSIZE.size_20,
+    color: COLORS.primaryWhiteHex,
+  },
+});
+
+export default HeaderBar;
---
+++ src/components/ProfilePic.jsx
@@ -0,0 +1,31 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { COLORS, SPACING } from "../theme/theme";
+
+const ProfilePic = () => {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source={require("../assets/app_images/avatar.png")}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: SPACING.space_12,
+    borderWidth: 2,
+    borderColor: COLORS.secondaryDarkGreyHex,
+    alignItems: "center",
+    justifyContent: "center",
+    overflow: "hidden",
+  },
+  image: {
+    height: SPACING.space_36,
+    width: SPACING.space_36,
+  },
+});
+
+export default ProfilePic;
