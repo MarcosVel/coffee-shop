2110f987e989edecc54e840a64e230c80ba82667
MarcosVel <marcosvelosodev@gmail.com>
2024-04-01 18:04:33 -0300

    refactor: component for order history item with quantity

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,7 @@ export { default as CustomIcon } from "./CustomIcon";
 export { default as EmptyList } from "./EmptyList";
 export { default as HeaderBar } from "./HeaderBar";
 export { default as ImageBGInfo } from "./ImageBGInfo";
+export { default as OrderHistoryItem } from "./OrderHistoryItem";
 export { default as PaymentFooter } from "./PaymentFooter";
 export { default as PaymentMethod } from "./PaymentMethod";
 export { default as PopUpAnimation } from "./PopUpAnimation";
--- src/screens/OrderHistory.jsx
+++ src/screens/OrderHistory.jsx
@@ -1,61 +1,9 @@
-import { LinearGradient } from "expo-linear-gradient";
-import {
-  FlatList,
-  Image,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
-import { HeaderBar } from "../components";
+import { FlatList, SafeAreaView, StyleSheet } from "react-native";
+import { EmptyList, HeaderBar, OrderHistoryItem } from "../components";
 import { useStore } from "../store/store";
-import { COLORS, FONTFAMILY } from "../theme/theme";
+import { COLORS } from "../theme/theme";
 
-const Order = ({ item }) => (
-  <View style={styles.order}>
-    <View style={styles.header}>
-      <View>
-        <Text style={styles.title}>Order Date</Text>
-        <Text style={styles.date}>{item.OrderDate}</Text>
-      </View>
-      <View>
-        <Text style={styles.title}>Total Amount</Text>
-        <Text style={styles.total}>${item.CartListPrice.toFixed(2)}</Text>
-      </View>
-    </View>
-
-    <View style={{ gap: 20 }}>
-      {item.Orders.map((orderItem) => {
-        console.log("orderItem", orderItem);
-        return orderItem.CartList.map((cartItem) => (
-          <LinearGradient
-            key={cartItem.id}
-            style={styles.orderItem}
-            start={{ x: 0, y: 0 }}
-            end={{ x: 1, y: 1 }}
-            colors={[COLORS.primaryGreyHex, COLORS.primaryBlackHex]}
-          >
-            <View style={styles.orderItemHeader}>
-              <Image source={cartItem.imagelink_square} style={styles.image} />
-              <View>
-                <Text style={styles.orderItemName}>{cartItem.name}</Text>
-                <Text style={styles.special_ingredient}>
-                  {cartItem.special_ingredient}
-                </Text>
-              </View>
-              <Text style={styles.orderItemPrice}>
-                <Text style={{ color: COLORS.primaryOrangeHex }}>$ </Text>
-                {parseFloat(cartItem.ItemPrice).toFixed(2)}
-              </Text>
-            </View>
-          </LinearGradient>
-        ));
-      })}
-    </View>
-  </View>
-);
-
-const OrderHistory = () => {
+const OrderHistory = ({ navigation }) => {
   const OrderHistoryList = useStore((state) => state.OrderHistoryList);
   const cleanOrderHistory = useStore((state) => state.cleanOrderHistory);
 
@@ -81,8 +29,6 @@ const OrderHistory = () => {
 
   const transformedArray = Object.values(groupedOrders);
 
-  console.log("transformedArray", transformedArray);
-
   return (
     <SafeAreaView style={styles.safeArea}>
       <HeaderBar title="Order History" onPress={() => cleanOrderHistory()} />
@@ -90,8 +36,11 @@ const OrderHistory = () => {
       <FlatList
         data={transformedArray}
         keyExtractor={(item) => item.OrderDate}
-        renderItem={({ item }) => <Order item={item} />}
+        renderItem={({ item }) => (
+          <OrderHistoryItem item={item} navigation={navigation} />
+        )}
         contentContainerStyle={styles.list}
+        ListEmptyComponent={() => <EmptyList screen="Your history" />}
       />
     </SafeAreaView>
   );
@@ -107,61 +56,6 @@ const styles = StyleSheet.create({
     paddingHorizontal: 32,
     paddingBottom: 96,
     gap: 24,
-  },
-  order: {},
-  header: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    marginBottom: 8,
-  },
-  title: {
-    color: COLORS.primaryWhiteHex,
-    fontSize: 14,
-    fontFamily: FONTFAMILY.poppins_semibold,
-  },
-  date: {
-    color: COLORS.primaryWhiteHex,
-    fontSize: 14,
-    fontFamily: FONTFAMILY.poppins_light,
-  },
-  total: {
-    color: COLORS.primaryOrangeHex,
-    fontSize: 14,
-    fontFamily: FONTFAMILY.poppins_regular,
-    textAlign: "right",
-  },
-  orderItem: {
-    padding: 16,
-    paddingTop: 12,
-    borderRadius: 24,
-  },
-  orderItemHeader: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    gap: 16,
-  },
-  image: {
-    width: 60,
-    height: 60,
-    borderRadius: 16,
-  },
-  orderItemName: {
-    color: COLORS.primaryWhiteHex,
-    fontSize: 16,
-    fontFamily: FONTFAMILY.poppins_regular,
-  },
-  special_ingredient: {
-    color: COLORS.primaryWhiteHex,
-    fontSize: 12,
-    fontFamily: FONTFAMILY.poppins_light,
-  },
-  orderItemPrice: {
-    color: COLORS.primaryWhiteHex,
-    fontSize: 20,
-    fontFamily: FONTFAMILY.poppins_semibold,
-    marginLeft: "auto",
   },
 });
 
